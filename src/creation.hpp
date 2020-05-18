ee7d07e0801c7f87fb54c1819f7e45cabc3db742
Kai <poi912713@gmail.com>
2020-05-18 18:37:03 +0800

    cmt518-2

--- src/creation.hpp
+++ src/creation.hpp
@@ -2,8 +2,9 @@
  * @ Author: Kai Xu
  * @ Create Time: 2020-05-16 16:47:48
  * @ Modified by: Kai Xu
- * @ Modified time: 2020-05-18 11:52:44
+ * @ Modified time: 2020-05-18 18:35:51
  * @ Description: create quadtree structure from input HEVC dense image.
+ *                This code is largely based on octnet.
  */
 #ifndef CREATION
 #define CREATION
@@ -11,22 +12,104 @@
 #include "commdef.hpp"
 #include "quadtree.hpp"
 
+class QuadtreeCreateHelperCpu
+{
+public:
+    QuadtreeCreateHelperCpu(ms::int grid_depth_, ms::int grid_height_, ms::int grid_width_) : grid_depth(grid_depth_), grid_height(grid_height_), grid_width(grid_width_), off_gd(0), off_gh(0), off_gw(0)
+    {
+    }
+    virtual ~QuadtreeCreateHelperCpu() {}
+
+    virtual void update_offsets(int off_gd_, int off_gh_, int off_gw_)
+    {
+        off_gd = off_gd_;
+        off_gh = off_gh_;
+        off_gw = off_gw_;
+        // printf("updated helper, off: %d,%d,%d\n", off_gd, off_gh, off_gw);
+    }
+
+    virtual void update_grid_coords(int &gd, int &gh, int &gw)
+    {
+        gd += off_gd;
+        gh += off_gh;
+        gw += off_gw;
+    }
+
+    virtual int get_grid_idx(int gd, int gh, int gw)
+    {
+        int old_grid_idx = (gd * grid_height + gh) * grid_width + gw;
+        return old_grid_idx;
+    }
+
+public:
+    ms::int grid_depth;
+    ms::int grid_height;
+    ms::int grid_width;
+
+    int off_gd;
+    int off_gh;
+    int off_gw;
+};
+
 namespace ms
 {
     class CreateFromDense
     {
     public:
-        CreateFromDense(qt_size_t h, qt_size_t d, qt_size_t f, const qt_data_t *data_ptr) : grid_height(h), grid_width(d), feature_size(f), data(data_ptr){};
+        CreateFromDense(int h, int w, int f, const qt_data_t *data_ptr) : grid_height(h), grid_width(w), feature_size(f), data(data_ptr), off_gh(0), off_gw(0)
+        {
+            grids = new quadtree(1, grids_height, grids_width, feature_size);
+        };
         ~CreateFromDense();
-        quadtree &operator()(bool fit = false, int fit_multiply = 1, bool pack = false, int n_threads = 1);
+        quadtree *operator()(bool fit = false, int fit_multiply = 1, bool pack = false, int n_threads = 1);
 
     private:
         quadtree &create_octree(bool fit, int fit_multiply, bool pack, int n_threads);
-        const qt_size_t grid_height;
-        const qt_size_t grid_width;
-        const qt_size_t feature_size;
+
+        quadtree *alloc_grid();
+        void create_quadtree_structure(QuadtreeCreateHelperCpu *helper);
+        void fit_quadtree(quadtree *grid, int fit_multiply, QuadtreeCreateHelperCpu *helper);
+        void pack_quadtree(quadtree *grid, QuadtreeCreateHelperCpu *helper);
+        void update_and_resize_quadtree(quadtree *grid);
+        void fill_quadtree_data(quadtree *grid, bool packed, QuadtreeCreateHelperCpu *helper);
+
+        bool is_occupied(float cx, float cy, float cz, float vd, float vh, float vw, int gd, int gh, int gw, QuadtreeCreateHelperCpu *helper);
+        void get_data(bool oc, float cx, float cy, float cz, float vd, float vh, float vw, int gd, int gh, int gw, QuadtreeCreateHelperCpu *helper, qt_data_t *dst);
+
+        void update_offsets(int off_gh_, int off_gw_)
+        {
+            off_gh = off_gh_;
+            off_gw = off_gw_;
+            // printf("updated helper, off: %d,%d,%d\n", off_gd, off_gh, off_gw);
+        }
+
+        void update_grid_coords(int &gh, int &gw)
+        {
+            gh += off_gh;
+            gw += off_gw;
+        }
+
+        int get_grid_idx(int gh, int gw)
+        {
+            int old_grid_idx = gh * grid_width + gw;
+            return old_grid_idx;
+        }
+
+    public:
+        const int grid_height;
+        const int grid_width;
+        const int feature_size;
         const qt_data_t *data;
+
+        int off_gh;
+        int off_gw;
+
+        quadtree *grids;
     };
+
+    void CreateFromDense::create_quadtree_structure(QuadtreeCreateHelperCpu *helper)
+    {
+    }
 } // namespace ms
 
 #endif
--- src/quadtree.hpp
+++ src/quadtree.hpp
@@ -2,7 +2,7 @@
  * @ Author: Kai Xu
  * @ Create Time: 2020-05-16 16:46:45
  * @ Modified by: Kai Xu
- * @ Modified time: 2020-05-18 11:22:11
+ * @ Modified time: 2020-05-18 18:00:57
  * @ Description:
  */
 #ifndef QUADTREE
@@ -13,9 +13,31 @@ namespace ms
     typedef int qt_size_t;
     typedef int qt_tree_t;
     typedef float qt_data_t;
+    const int N_TREE_INTS = 2;
+
     struct quadtree
     {
     public:
+        quadtree(qt_size_t _n = 0,
+                 qt_size_t _grid_height = 0,
+                 qt_size_t _grid_width = 0,
+                 qt_size_t _feature_size = 0,
+                 qt_size_t _n_leafs = 0)
+            : n(_n), grid_height(_grid_height), grid_width(_grid_width),
+              feature_size(_feature_size), n_leafs(_n_leafs),
+              grid_capacity(_n * _grid_height * _grid_width),
+              data_capacity(_n_leafs * _feature_size)
+        {
+            trees = new qt_tree_t[grid_capacity * N_TREE_INTS];
+            prefix_leafs = new qt_size_t[grid_capacity];
+            data = new qt_data_t[data_capacity];
+        };
+
+        void resize(qt_size_t _n, qt_size_t _grid_height, qt_size_t _grid_width, qt_size_t _feature_size, qt_size_t _n_leafs);
+
+        ~quadtree();
+
+    private:
         qt_size_t n;            ///< number of grid-quadtrees (batch size).
         qt_size_t grid_height;  ///< number of shallow quadtrees in the height dimension.
         qt_size_t grid_width;   ///< number of shallow quadtrees in the width dimension.
@@ -23,7 +45,7 @@ namespace ms
 
         qt_size_t n_leafs; ///< number of leaf nodes in the complete struct.
 
-        qt_tree_t *trees;        ///< array of length quadtree_num_blocks(grid) x N_TREE_INTS that encode the structure of the shallow quadtrees as bit strings.
+        qt_tree_t *trees;        ///< array of length quadtree_num_blocks(grid) x N_TREE_qt_size_tS that encode the structure of the shallow quadtrees as bit strings.
         qt_size_t *prefix_leafs; ///< prefix sum of the number of leafs in each shallow quadtree.
         qt_data_t *data;         ///< contiguous data array, all feature vectors associated with the grid-quadtree data structure.
 
@@ -31,6 +53,49 @@ namespace ms
         qt_size_t data_capacity; ///< Indicates how much memory is allocated for the data array
     };
 
+    inline void quadtree::resize(qt_size_t _n, qt_size_t _grid_height, qt_size_t _grid_width, qt_size_t _feature_size, qt_size_t _n_leafs)
+    {
+        n = _n;
+        grid_height = _grid_height;
+        grid_width = _grid_width;
+        feature_size = _feature_size;
+        n_leafs = _n_leafs;
+
+        qt_size_t _grid_capacity = n * grid_height * grid_width;
+
+        if (grid_capacity < _grid_capacity)
+        {
+            grid_capacity = _grid_capacity;
+            if (trees != nullptr)
+            {
+                delete[] trees;
+            }
+            trees = new qt_tree_t[grid_capacity * N_TREE_INTS];
+
+            if (prefix_leafs != nullptr)
+            {
+                delete[] prefix_leafs;
+            }
+            prefix_leafs = new qt_size_t[grid_capacity];
+        }
+        qt_size_t _data_capacity = n_leafs * feature_size;
+        if (data_capacity < _data_capacity)
+        {
+            data_capacity = _data_capacity;
+            if (data != nullptr)
+            {
+                delete[] data;
+            }
+            data = new qt_data_t[data_capacity];
+        }
+    }
+
+    inline quadtree::~quadtree()
+    {
+        delete[] trees;
+        delete[] prefix_leafs;
+        delete[] data;
+    }
 } // namespace ms
 
 #endif
